f1dc9cf59fdbf13633ed3036ccf85ab783d5d2c3
hainguyen710 <HaiNHTH2110016@fpt.edu.vn>
2022-08-29 09:13:24 +0700

    2

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted
+{
+    internal class Class1
+    {
+        public static void Main()
+        {
+            SortedList objSL = new SortedList();
+            objSL.Add("4", "!");
+            objSL.Add("3","Brilliant");
+            objSL.Add("2", "am");
+            objSL.Add("1", "I");
+
+            Console.WriteLine("Number of elements in objSl: {0}",objSL.Count);
+            Console.WriteLine("\t-KEY-\t-VALUE-");
+            for(int i = 0; i < objSL.Count; i++)
+            {
+                Console.WriteLine("\t{0}:\t{1}", objSL.GetKey(i));
+                objSL.GetByIndex(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class Product
+    {
+        string name;
+        double cost;
+        int onhand;
+        public Product(string n,double c, int h)
+        {
+           name = n;
+            cost = c;
+            onhand = h;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,-10}Cost: {1,6:C} On hand: {2}",name,cost,onhand); 
+
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList inv = new ArrayList();
+            //Add elements to the list
+
+            inv.Add(new Product("A", 5.9, 3)); //Product p1 = new Product(,,);
+            inv.Add(new Product("B", 8.2, 2));
+            inv.Add(new Product("C", 3.5, 4));
+            inv.Add(new Product("D", 1.8, 8));
+            Console.WriteLine("Product list:");
+            foreach (Product i in inv)
+            {
+                Console.WriteLine("" + i);
+            }
+        }
+    }
+}
